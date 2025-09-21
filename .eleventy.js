c7f07db0c65ec4c7714a960384b144faee3c1dbf
gofernie <chris@crump.ca>
2025-09-20 21:06:16 -0600

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,13 @@
 // .eleventy.js
 module.exports = function (eleventyConfig) {
-  // Copy images through to _site
   eleventyConfig.addPassthroughCopy("src/img");
 
-  // Optional: allow both styles of referencing the base layout
-  // So either `layout: base.njk` or `layout: layout/base.njk` will resolve.
+  // Optional aliases (helps if some pages still say layout/base.njk)
   eleventyConfig.addLayoutAlias("base", "base.njk");
   eleventyConfig.addLayoutAlias("layout/base.njk", "base.njk");
 
   return {
-    dir: {
-      input: "src",
-      includes: "_includes",  // single includes dir
-      output: "_site",
-    },
+    dir: { input: "src", includes: "_includes", output: "_site" },
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
     templateFormats: ["njk", "md", "html"],
