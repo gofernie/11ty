7305fd7768761d8737488ff0558e6670a872ab3d
gofernie <chris@crump.ca>
2025-09-20 21:26:17 -0600

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,20 @@
+// .eleventy.js
 module.exports = function (eleventyConfig) {
+  // Copy images (so /img/... works after deploy)
   eleventyConfig.addPassthroughCopy("src/img");
+
+  // If any old pages still say layout: layout/base.njk, map it to base.njk
   eleventyConfig.addLayoutAlias("base", "base.njk");
   eleventyConfig.addLayoutAlias("layout/base.njk", "base.njk");
+
   return {
-    dir: { input: "src", includes: "_includes", output: "_site" },
+    dir: {
+      input: "src",
+      includes: ".",     // 👈 LOOK HERE INSTEAD OF _includes
+      output: "_site",
+    },
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
-    templateFormats: ["njk","md","html"],
+    templateFormats: ["njk", "md", "html"],
   };
 };
